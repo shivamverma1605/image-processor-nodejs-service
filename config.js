46e21254d0ba6cf2c02356b0c9d5c247e7f78509
shivam <shivam.verma_cs19@gla.ac.in>
2025-04-01 10:47:30 +0530

    config updated

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     webhookURL: process.env.WEBHOOK_URL,
-    port: process.env.PORT || 5000
+    port: process.env.PORT || 5000,
+    environment:dev
 };
